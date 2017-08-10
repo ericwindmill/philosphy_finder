4e6d113b34d363ecc249344002a01e5a11b0c147
Eric Windmill <ericwindmill@gmail.com>
2017-08-10 11:01:26 -0700

    find first link

--- dist/bundle.js
+++ dist/bundle.js
@@ -107,17 +107,56 @@ async function WikipediaNode (page) {
 
 "use strict";
 /* harmony export (immutable) */ __webpack_exports__["a"] = parseResults;
+/* unused harmony export checkLink */
 async function parseResults (results) {
+  if (results.parse.title === 'Philosophy') {
+    console.log('found philosophy')
+    return true
+  }
+
   let shadowDom = document.querySelector('#shadowDom')
-  shadowDom.innerHTML = `<p>${results.parse.text['*']}</p>`
-  console.log(shadowDom)
+  shadowDom.innerHTML = `${results.parse.text['*']}`
+
+  function findFirstLink (i) {
+    let wikiDom = document.getElementsByClassName('mw-parser-output')[0]
+    let articleFirstBodyParagraph = wikiDom.querySelector('p > b').parentElement
+    let potentialFirstLink = articleFirstBodyParagraph.getElementsByTagName('a')[i]
+    var url = potentialFirstLink.getAttribute('href')
+    return url
+  }
+
+  function checkLink (link) {
+    const goodLink = (
+      link.includes('wiki') &&
+      !link.includes('Help:') &&
+      !link.includes('File') &&
+      !link.includes('Wikipedia') &&
+      !link.includes('wiktionary') &&
+      !link.includes('Latin')
+    )
+    return goodLink
+  }
+
+  let i = 0
+  let found = false
+  let link
+  while (!found) {
+    link = await findFirstLink(i)
+    if (checkLink(link)) {
+      found = true
+    } else {
+      i++
+    }
+  }
+  console.log(link)
+  return link
 }
-//     let returnJson
-//     fetchButton.addEventListener('click', async (e) => {
-//       e.preventDefault()
-//       returnJson = await WikipediaNode('dragon')
-//       result.innerHTML = `<p><pre><code>${returnJson.parse.text['*']}</code></pre></p>`
-//     })
+
+async function checkLink (link) {
+
+}
+
+
 
 /***/ })
 /******/ ]);
