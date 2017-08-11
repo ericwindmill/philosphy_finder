dd22229934bfb69b8950a9c41545ad0d9f70a349
Eric Windmill <ericwindmill@gmail.com>
2017-08-11 15:12:59 -0700

    Total Refactor because of fauty edge cases

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './lib/getting-to-philosophy.js',
+  entry: './lib/gettingToPhilosophy.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
